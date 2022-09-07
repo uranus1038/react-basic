63d168a27587e20e3ade87c7c79561af2869df71
Anusorn <anusorn1039@gmail.com>
2022-09-08 00:00:31 +0700

    Add new react-basic

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+//Component 
+import Navbar from './Component/Navbar.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import "../css/Navbar.css";
+function Navbar()
+{
+    return (
+    <div>
+        <h2 className='mainHeader'><a href="/sk">Brand</a></h2>
+    </div>
+    );
+}
+
+export default Navbar ;
